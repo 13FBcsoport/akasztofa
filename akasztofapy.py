63c516342b6accfc5c9917939252c73e1e8485e3
FazekasBalint <balint.lipot@gmail.com>
2023-11-30 11:25:23 +0100

    beolvas fuggveny

--- akasztofapy.py
+++ akasztofapy.py
@@ -1,2 +1,9 @@
 print('akasztofa')
 uzenet=input("Kérek egy betut")
+
+
+def beolvas(fajlnev:str)->list:
+    fajl=open(fajlnev,"r" , encoding=("utf-8"))
+    for f in fajl:
+        f.split(",")
+    fajl.close()
