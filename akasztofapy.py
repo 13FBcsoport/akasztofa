d39478a74afc1f7a0326672346c6838a8d912da0
Gintli Máté <gintli.mate@students.jedlik.eu>
2023-12-07 11:32:55 +0100

    kodiras2

--- akasztofapy.py
+++ akasztofapy.py
@@ -1,14 +1,25 @@
 from random import choice
+
 def beolvas(fajlnev)->list:
     szavak_listaja=[]
     fajl=open(fajlnev,"r" , encoding=("utf-8"))
     for szo in fajl:
-        szavak_listaja.append(szo.strip)
-        print(szo.strip("\n"))
+        szavak_listaja.append(szo.strip("\n"))
     
     fajl.close()
     return szavak_listaja
 
-def randomszo(szavak: list):
-    szo = choice(szavak)
+def sorsolo(lista):
+    szo = choice(lista)
     return szo
+
+def betuhely_kiiro(szo, kar):
+    visszaad = ''
+    for betu in szo:
+        if betu.lower() == kar.lower():
+            print(betu, end=" ")
+            visszaad += betu
+        else:
+            print("_", end=" ")
+            visszaad += '_'
+    return visszaad
--- main.py
+++ main.py
@@ -1,3 +1,11 @@
 from akasztofapy import *
+szavak = []
+szavak=beolvas("szavak.txt")
 
-print(beolvas("szavak.txt"))
+random_szo = sorsolo(szavak)
+print(random_szo)
+
+kar = input("Kérem a betűt: ")
+
+vissza = betuhely_kiiro(random_szo, kar)
+print(vissza)
