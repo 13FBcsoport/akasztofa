a46dbb1b29f714254fd785ac22bcd887ea79b117
Gintli-Mate <gintli.mate@students.jedlik.eu>
2023-12-13 16:37:29 +0100

    kód javítása

--- akasztofapy.py
+++ akasztofapy.py
@@ -13,10 +13,10 @@ def sorsolo(lista):
     szo=choice(lista)
     return szo
 
-def betu_hely_kiiro(szo, kar):
+def betu_hely_kiiro(szo, talalgatasok):
     visszaad = ""
     for betu in szo:
-        if betu.lower() == kar.lower():
+        if betu.lower() in [t.lower() for t in talalgatasok]:
             print(betu, end=" ")
             visszaad += betu
         else:
--- main.py
+++ main.py
@@ -1,19 +1,38 @@
 from akasztofapy import *
-szavak = []
-szavak = beolvas("szavak.txt")
 
-random_szo = sorsolo(szavak)
-print(random_szo)
+def jatek():
+    szavak = beolvas("szavak.txt")
 
-kar = input("Kérem a betűt: ")
+    random_szo = sorsolo(szavak)
+    print("A kiválasztott szó:", "_" * len(random_szo))
 
-szavak = []
-szavak = beolvas("szavak.txt")
+    min_probalkozasok = len(random_szo) + 3
+    probalkozasok = min_probalkozasok
+    talalgatasok = []
+    rossz_tippek = set()
+    
+    while probalkozasok > 0:
+        betu = input("Kérek egy betűt: ")
 
-random_szo = sorsolo(szavak)
-print(random_szo)
+        if betu.lower() in random_szo.lower():
+            probalkozasok -= 1
+        else:
+            probalkozasok -= 1
+            print("Rossz tipp! Próbálkozások száma:", probalkozasok)
+            rossz_tippek.add(betu)
+            print("Eddigi rossz tippek:", ", ".join(rossz_tippek))
+            continue
 
-betu = input("Kérek egy betűt: ")
+        talalgatasok.append(betu)
+        vissza = betu_hely_kiiro(random_szo, talalgatasok)
+        
+        print("Eddigi találgatások:", ", ".join(talalgatasok))
+        print("Eddigi találgatások eredménye:", vissza)
 
-vissza = betu_hely_kiiro(random_szo,betu)
-print(vissza)
+        if "_" not in vissza:
+            print("Gratulálok, megfejtetted a szót!")
+            return
+    
+    print("Sajnálom, vesztettél. A helyes szó:", random_szo)
+
+jatek()
